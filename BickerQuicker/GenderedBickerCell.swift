f24069f05ae3813b40877dda93907e026f1382bc
Brendan Raftery <braftery@purdue.edu>
2018-03-05 18:23:05 -0500

    Updated bicker stuff

---
+++ BickerQuicker/GenderedBickerCell.swift
@@ -0,0 +1,24 @@
+//
+//  GenderedBickerCellTableViewCell.swift
+//  BickerQuicker
+//
+//  Created by Brendan Raftery on 3/1/18.
+//  Copyright © 2018 Jonathan Grider. All rights reserved.
+//
+
+import UIKit
+
+class GenderedBickerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
