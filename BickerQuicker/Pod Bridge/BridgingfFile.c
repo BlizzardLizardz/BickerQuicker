18d25f0f90d3cd474f5476e93c06330e2f220ff0
Harjas Monga <harjasmonga@gmail.com>
2018-03-07 18:28:29 -0500

    added pod bridging file

---
+++ BickerQuicker/Pod Bridge/BridgingfFile.c
@@ -0,0 +1,9 @@
+//
+//  BridgingfFile.c
+//  BickerQuicker
+//
+//  Created by Harjas Monga on 3/7/18.
+//  Copyright © 2018 Jonathan Grider. All rights reserved.
+//
+
+#include "BridgingfFile.h"
---
+++ BickerQuicker/Pod Bridge/BridgingfFile.h
@@ -0,0 +1,14 @@
+//
+//  BridgingfFile.h
+//  BickerQuicker
+//
+//  Created by Harjas Monga on 3/7/18.
+//  Copyright © 2018 Jonathan Grider. All rights reserved.
+//
+
+#ifndef BridgingfFile_h
+#define BridgingfFile_h
+
+#include <stdio.h>
+
+#endif /* BridgingfFile_h */
